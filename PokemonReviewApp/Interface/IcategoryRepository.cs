412c3e6518a7b319e9387dc9b0ee960dc68668f6
Akapaya <63330951+Akapaya@users.noreply.github.com>
2024-02-21 10:08:44 -0300

    Added Category Repository and Controller
    Added Category Repository and Controller

---
+++ PokemonReviewApp/Interface/IcategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Interface
+{
+    public interface IcategoryRepository
+    {
+        ICollection<Category> GetCategories();
+        Category GetCategory(int id);
+        ICollection<Pokemon> GetPokemonsByCategory(int categoryId);
+        bool CategoryExists(int categoryId);
+    }
+}
---
+++ PokemonReviewApp/Repository/CategoryRepository.cs
@@ -0,0 +1,36 @@
+﻿using PokemonReviewApp.Data;
+using PokemonReviewApp.Interface;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Repository
+{
+    public class CategoryRepository : IcategoryRepository
+    {
+        private readonly DataContext _context;
+
+        public CategoryRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public bool CategoryExists(int categoryId)
+        {
+            return _context.Categories.Any(c => c.Id == categoryId);
+        }
+
+        public ICollection<Category> GetCategories()
+        {
+            return _context.Categories.ToList();
+        }
+
+        public Category GetCategory(int id)
+        {
+            return _context.Categories.Where(c => c.Id == id).FirstOrDefault();
+        }
+
+        public ICollection<Pokemon> GetPokemonsByCategory(int categoryId)
+        {
+            return _context.PokemonCategories.Where(c => c.CategoryId == categoryId).Select(c=>c.Pokemon).ToList();
+        }
+    }
+}
