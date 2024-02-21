f76874ccaebaf3a1449fca34ab6e6ed4d04d8cab
Akapaya <63330951+Akapaya@users.noreply.github.com>
2024-02-21 10:08:05 -0300

    Added Pokemon Repository and Controller
    Added Pokemon Repository and Controller

---
+++ PokemonReviewApp/Repository/PokemonRepository.cs
@@ -0,0 +1,54 @@
+﻿using PokemonReviewApp.Data;
+using PokemonReviewApp.Interface;
+using PokemonReviewApp.Models;
+using System.Xml.Linq;
+
+namespace PokemonReviewApp.Repository
+{
+    public class PokemonRepository : IPokemonRepository
+    {
+        private readonly DataContext _context;
+
+        public PokemonRepository(DataContext context) 
+        {
+            _context = context;
+        }
+
+        public Pokemon GetPokemon(int id)
+        {
+            return _context.Pokemons.Where(p => p.Id == id).FirstOrDefault();
+        }
+
+        public Pokemon GetPokemon(string name)
+        {
+            return _context.Pokemons.Where(p => p.Name == name).FirstOrDefault();
+        }
+
+        public Pokemon GetPokemon(Pokemon pokemon)
+        {
+            return _context.Pokemons.Where(p => p == pokemon).FirstOrDefault();
+        }
+
+        public decimal GetPokemonRating(int pokeId)
+        {
+            var review = _context.Reviews.Where(p => p.Pokemon.Id == pokeId);
+
+            if(review.Count() <= 0)
+            {
+                return 0;
+            }
+            
+            return ((Decimal)review.Sum(r => r.Rating) / review.Count());
+        }
+
+        public ICollection<Pokemon> GetPokemons()
+        {
+            return _context.Pokemons.OrderBy(p => p.Id).ToList();
+        }
+
+        public bool PokemonExists(int pokeId)
+        {
+            return _context.Pokemons.Any(p => p.Id == pokeId);
+        }
+    }
+}
